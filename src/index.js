16969a3ac725d140624b65ed804462d07a05cb05
Kacper Łukawski <ka.lukawski@gmail.com>
2017-01-19 20:38:41 +0100

    Basic class structure

--- src/index.js
+++ src/index.js
@@ -7,5 +7,28 @@ class Pomodoro {
   getSession () {
     return this.session
   }
+
+  getPause () {
+    return this.pause
+  }
+
+  setSession (length) {
+    this.session = length
+  }
+
+  setPause (length) {
+    this.pause = length
+  }
+
+  startSession () {
+
+  }
+
+  startPause () {
+
+  }
 }
 
+document.addEventListener('DOMContentLoaded', function (e) {
+  var clock = new Pomodoro(25, 5)
+})
