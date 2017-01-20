8c0f60db3a81b8ead314ed7835e4e965098031b0
Kacper Łukawski <ka.lukawski@gmail.com>
2017-01-20 20:51:46 +0100

    Counter is wworking

--- src/index.js
+++ src/index.js
@@ -19,16 +19,28 @@ class Pomodoro {
   setPause (length) {
     this.pause = length
   }
-
-  startSession () {
-
-  }
-
-  startPause () {
-
-  }
 }
 
 document.addEventListener('DOMContentLoaded', function (e) {
-  var clock = new Pomodoro(25, 5)
+  var clock = new Pomodoro(15, 300)
+  var sessionInterval = 0
+  var pauseInterval = 0
+  var leftBox = document.getElementById('left')
+
+  document.getElementById('start').addEventListener('click', function () {
+    let counter = clock.getSession()
+    let minutes = 0
+    let seconds = 0
+    sessionInterval = setInterval(function () {
+      counter--
+      minutes = (Math.floor(counter / 60) < 10) ? '0' + Math.floor(counter / 60) : Math.floor(counter / 60)
+      seconds = (counter % 60 < 10) ? '0' + counter % 60 : counter % 60
+      leftBox.innerText = minutes + ':' + seconds
+      if (counter === 0) clearInterval(sessionInterval)
+    }, 1000)
+  })
+
+  document.getElementById('stop').addEventListener('click', function () {
+    clearInterval(sessionInterval)
+  })
 })
